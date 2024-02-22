66fc48dfa56f693540b2956dcac91cb6d1b6aaaf
Goodbird <goodbirdmail@yandex.ru>
2024-02-22 23:37:20 +0300

    Added missing molang elements and mclib parts

---
+++ src/main/java/com/eliotlash/molang/functions/AtanDegrees.java
@@ -0,0 +1,19 @@
+package com.eliotlash.molang.functions;
+
+
+import software.bernie.shadowed.eliotlash.mclib.math.IValue;
+import software.bernie.shadowed.eliotlash.mclib.math.functions.classic.ATan;
+
+public class AtanDegrees extends ATan
+{
+    public AtanDegrees(IValue[] values, String name) throws Exception
+    {
+        super(values, name);
+    }
+
+    @Override
+    public double get()
+    {
+        return super.get() / Math.PI * 180;
+    }
+}
---
+++ src/main/java/mchorse/blockbuster/client/particles/components/GuiModelRenderer.java
@@ -0,0 +1,8 @@
+package mchorse.blockbuster.client.particles.components;
+
+public class GuiModelRenderer {
+    private static boolean isRendering = false;
+    public static boolean isRendering(){
+        return isRendering;
+    }
+}
